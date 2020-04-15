27b11353257800bd28bd138a467dd8dcedf2b70a
J Proctor <jproctor@mtholyoke.edu>
2020-04-15 10:26:21 -0400

    Prep for first test on mars

--- mirror_pds.sh
+++ mirror_pds.sh
@@ -5,14 +5,14 @@ set -e
 REMOTE_PREPROCESSED=http://pds-geosciences.wustl.edu/msl/msl-m-chemcam-libs-4_5-rdr-v1/mslccm_1xxx/
 MASTER_FILE=msl_ccam_obs.csv
 
-MSL_DIR=/srv/nfs/common/spectra/MSL_PDS
+MSL_DIR=/mars/g/rch3/r/mdyar/jproctor-test/MSL_PDS
 CCS_DATA_DIR=$MSL_DIR/ccs_data
 CCS_PREFIX=$CCS_DATA_DIR/ccs
 CCS_ORIGINALS=$CCS_DATA_DIR/original
 MODELS_PREDS_DIR=$MSL_DIR/models_predictions
 MOC_DIR=$MODELS_PREDS_DIR/moc
 
-LIBS_DATA_DIR=/srv/nfs/common/spectra/LIBS
+LIBS_DATA_DIR=/mars/g/rch3/r/mdyar/jproctor-test/LIBS
 LIBS_MIX_FILE=$LIBS_DATA_DIR/mhc_doping+mhc_big+lanl_400+public_caltargets.npz
 LANL_FILE=$LIBS_DATA_DIR/lanl_new.hdf5
 
@@ -27,24 +27,19 @@ EOF
 endtime=$(date +%s)
 echo "$endtime: Download finished after $((endtime - starttime)) seconds."
 
-# Add the new CCS files to the server-readable data
-python2.7 process_msl_files.py -o $CCS_PREFIX -i $CCS_ORIGINALS \
-    -m $CCS_DATA_DIR/$MASTER_FILE
-echo "$(date +%s): Added new CCS files to ${CCS_PREFIX}.xxx.hdf5"
-
-# Predict the new shots
-python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR \
-    --model standard --lanl-file $LANL_FILE
-echo "$(date +%s): Predicted compositions for new CCS files"
-
-python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR  \
-    --model mixed --libs-mix-file $LIBS_MIX_FILE
-echo "$(date +%s): Predicted compositions for new CCS files with Mixed model"
-
-python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR \
-    --model moc --moc-dir $MOC_DIR
-echo "$(date +%s): Predicted compositions for new CCS files with MOC model"
-
-python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR \
-    --model dust
-echo "$(date +%s): Predicted dust shots for new CCS files with the Dust Classifier"
+# # Add the new CCS files to the server-readable data
+# python2.7 process_msl_files.py -o $CCS_PREFIX -i $CCS_ORIGINALS -m $CCS_DATA_DIR/$MASTER_FILE
+# echo "$(date +%s): Added new CCS files to ${CCS_PREFIX}.xxx.hdf5"
+#
+# # Predict the new shots
+# python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR --model standard --lanl-file $LANL_FILE
+# echo "$(date +%s): Predicted compositions for new CCS files"
+#
+# python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR  --model mixed --libs-mix-file $LIBS_MIX_FILE
+# echo "$(date +%s): Predicted compositions for new CCS files with Mixed model"
+#
+# python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR --model moc --moc-dir $MOC_DIR
+# echo "$(date +%s): Predicted compositions for new CCS files with MOC model"
+#
+# python2.7 web_model.py -o $MODELS_PREDS_DIR --ccs-dir $CCS_DATA_DIR --model dust
+# echo "$(date +%s): Predicted dust shots for new CCS files with the Dust Classifier"
--- rsync_mhc.sh
+++ rsync_mhc.sh
@@ -1,68 +1,72 @@
 #!/usr/bin/env bash
 set -e
 
-REMOTE_SERVER=rover.mtholyoke.edu
-REMOTE_USER=tommy
-RSYNC='rsync -e ssh -zrlv --inplace'
+# REMOTE_SERVER=rover.mtholyoke.edu
+# REMOTE_USER=tommy
+# RSYNC='rsync -e ssh -zrlv --inplace'
 PYTHON='python2.7 -W i'
 
 # LIBS -------------------
 
-LIBS_LOCAL_DIR=/srv/nfs/common/spectra/LIBS/MHC
-LIBS_REMOTE_DIR=/mnt/research-space/MHC.LIBS/DATA
-
-echo "Syncing LIBS files..."
-remote_addr="$REMOTE_USER@$REMOTE_SERVER:$LIBS_REMOTE_DIR"
-$RSYNC "$remote_addr/prepro_no_blr*" "$LIBS_LOCAL_DIR/"
-echo "  done."
+LIBS_LOCAL_DIR=/mars/g/rch3/r/mdyar/jproctor-test/LIBS/MHC
+# LIBS_REMOTE_DIR=/mnt/research-space/MHC.LIBS/DATA
+#
+# echo "Syncing LIBS files..."
+# remote_addr="$REMOTE_USER@$REMOTE_SERVER:$LIBS_REMOTE_DIR"
+# $RSYNC "$remote_addr/prepro_no_blr*" "$LIBS_LOCAL_DIR/"
+# echo "  done."
 
 # SuperLIBS -------------------
 
-SUPERLIBS_LOCAL_DIR=/srv/nfs/common/spectra/SuperLIBS/MHC
-SUPERLIBS_REMOTE_DIR='/mnt/research-space/MHC.SUPERLIBS/SAMPLE\ RUNS'
-
-echo "Syncing SuperLIBS files..."
-remote_addr="$REMOTE_USER@$REMOTE_SERVER:$SUPERLIBS_REMOTE_DIR"
-$RSYNC "$remote_addr/prepro_no_blr*" "$SUPERLIBS_LOCAL_DIR/"
-echo "  done."
+# SUPERLIBS_LOCAL_DIR=/srv/nfs/common/spectra/SuperLIBS/MHC
+# SUPERLIBS_REMOTE_DIR='/mnt/research-space/MHC.SUPERLIBS/SAMPLE\ RUNS'
+#
+# echo "Syncing SuperLIBS files..."
+# remote_addr="$REMOTE_USER@$REMOTE_SERVER:$SUPERLIBS_REMOTE_DIR"
+# $RSYNC "$remote_addr/prepro_no_blr*" "$SUPERLIBS_LOCAL_DIR/"
+# echo "  done."
 
 # Raman ------------------
 
-RAMAN_LOCAL_DIR=/srv/nfs/common/spectra/Raman/MHC
-RAMAN_REMOTE_DIR=/mnt/research-space/rdata
-
-echo "Syncing Raman files..."
-remote_addr="$REMOTE_USER@$REMOTE_SERVER:$RAMAN_REMOTE_DIR"
-$RSYNC "$remote_addr/raman*" "$RAMAN_LOCAL_DIR/"
-echo "  done."
+# RAMAN_LOCAL_DIR=/srv/nfs/common/spectra/Raman/MHC
+# RAMAN_REMOTE_DIR=/mnt/research-space/rdata
+#
+# echo "Syncing Raman files..."
+# remote_addr="$REMOTE_USER@$REMOTE_SERVER:$RAMAN_REMOTE_DIR"
+# $RSYNC "$remote_addr/raman*" "$RAMAN_LOCAL_DIR/"
+# echo "  done."
 
 # Mossbauer ------------------
 
-MOSS_LOCAL_DIR=/srv/nfs/common/spectra/Mossbauer/MHC
-MOSS_REMOTE_DIR=/mnt/research-space/mdata
-
-echo "Syncing Mossbauer files..."
-remote_addr="$REMOTE_USER@$REMOTE_SERVER:$MOSS_REMOTE_DIR"
-$RSYNC "$remote_addr/mossbauer*" "$MOSS_LOCAL_DIR/"
-echo "  done."
+# MOSS_LOCAL_DIR=/srv/nfs/common/spectra/Mossbauer/MHC
+# MOSS_REMOTE_DIR=/mnt/research-space/mdata
+#
+# echo "Syncing Mossbauer files..."
+# remote_addr="$REMOTE_USER@$REMOTE_SERVER:$MOSS_REMOTE_DIR"
+# $RSYNC "$remote_addr/mossbauer*" "$MOSS_LOCAL_DIR/"
+# echo "  done."
 
 # XRF ------------------
 
-XRF_LOCAL_DIR=/srv/nfs/common/spectra/XRF
-XRF_REMOTE_DIR=/mnt/research-space/xdata
+XRF_LOCAL_DIR=/mars/g/rch3/r/mdyar/jproctor-test/XRF
+XRF_REMOTE_DIR=/mars/g/rch3/r/mdyar/data_pXRF
+# XRF_MASTER_FILE=
 
-echo "Syncing XRF files..."
-remote_addr="$REMOTE_USER@$REMOTE_SERVER:$XRF_REMOTE_DIR"
-$RSYNC "$remote_addr/tracer_17_csv/*" "$XRF_LOCAL_DIR/original"
-$RSYNC "$remote_addr/olympus_17_csv/*" "$XRF_LOCAL_DIR/original"
-$RSYNC "$remote_addr/$XRF_MASTER_FILE" "$XRF_LOCAL_DIR"
+echo "Copying XRF files..."
+# remote_addr="$REMOTE_USER@$REMOTE_SERVER:$XRF_REMOTE_DIR"
+# $RSYNC "$remote_addr/tracer_17_csv/*" "$XRF_LOCAL_DIR/original"
+# $RSYNC "$remote_addr/olympus_17_csv/*" "$XRF_LOCAL_DIR/original"
+# $RSYNC "$remote_addr/$XRF_MASTER_FILE" "$XRF_LOCAL_DIR"
+cp "$XRF_REMOTE_DIR/tracer_17_csv/*" "$XRF_LOCAL_DIR/original/"
+cp "$XRF_REMOTE_DIR/olympus_17_csv/*" "$XRF_LOCAL_DIR/original/"
+# cp "$XRF_REMOTE_DIR/$XRF_MASTER_FILE" "$XRF_LOCAL_DIR"
 echo "  done."
 
-echo "Processing new XRF files..."
-# TODO: instead of deleting, move to a backup dir. Then, if the python
-#       script fails, we can roll back.
-rm -f $XRF_LOCAL_DIR/xrf*
-$PYTHON process_xrf_files.py -o $XRF_LOCAL_DIR/xrf \
-	-i $XRF_LOCAL_DIR/original -m $XRF_LOCAL_DIR/$XRF_MASTER_FILE \
-	-m2 $LIBS_LOCAL_DIR/$LIBS_COMP_FILE
-echo "  done."
+# echo "Processing new XRF files..."
+# # TODO: instead of deleting, move to a backup dir. Then, if the python
+# #       script fails, we can roll back.
+# rm -f $XRF_LOCAL_DIR/xrf*
+# $PYTHON process_xrf_files.py -o $XRF_LOCAL_DIR/xrf \
+# 	-i $XRF_LOCAL_DIR/original -m $XRF_LOCAL_DIR/$XRF_MASTER_FILE \
+# 	-m2 $LIBS_LOCAL_DIR/$LIBS_COMP_FILE
+# echo "  done."
