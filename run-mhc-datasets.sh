f61a916bd69590498df9131e27ae6b91750de755
J Proctor <jproctor@mtholyoke.edu>
2020-05-13 09:39:07 -0400

    Enable ChemLIBS, SuperLIBS 5120, and nightly refresh

--- run-mhc-datasets.sh
+++ run-mhc-datasets.sh
@@ -30,30 +30,27 @@ rsync -av $RAMAN_DIR/to-DEVAS/ $NEMO_ROOT/Raman/MHC/ >> $RAMAN_LOG 2>&1
 echo "### $(date) - finished rsync" >> $RAMAN_LOG
 echo "Finished Raman"
 
-echo "Skipping ChemLIBS"
-# echo "Starting ChemLIBS"
+echo "Starting ChemLIBS"
 CHEMLIBS_DIR=$DATA_ROOT/MHC.LIBS/DATA
 CHEMLIBS_LOG=$CHEMLIBS_DIR/nightly-logs/chemlibs-$TODAY.log
-# echo "### $(date) - starting processing" > $CHEMLIBS_LOG
-# python2.7 $SCRIPT_ROOT/process_mhc_files.py -i $CHEMLIBS_DIR/PREPROCESSED_NO_BLR -o $CHEMLIBS_DIR/to-DEVAS/prepro_no_blr -m $CHEMLIBS_DIR/COMPOSITIONS/Millennium_COMPS.xlsx >> $CHEMLIBS_LOG 2>&1
-# echo "### $(date) - finished processing, starting rsync" >> $CHEMLIBS_LOG
-# rsync -av $CHEMLIBS_DIR/to-DEVAS/ $NEMO_ROOT/LIBS/MHC/ >> $CHEMLIBS_LOG 2>&1
-# echo "### $(date) - finished rsync" >> $CHEMLIBS_LOG
-# echo "Finished ChemLIBS"
+echo "### $(date) - starting processing" > $CHEMLIBS_LOG
+python2.7 $SCRIPT_ROOT/process_mhc_files.py -i $CHEMLIBS_DIR/PREPROCESSED_NO_BLR -o $CHEMLIBS_DIR/to-DEVAS/prepro_no_blr -m $CHEMLIBS_DIR/COMPOSITIONS/Millennium_COMPS.xlsx >> $CHEMLIBS_LOG 2>&1
+echo "### $(date) - finished processing, starting rsync" >> $CHEMLIBS_LOG
+rsync -av $CHEMLIBS_DIR/to-DEVAS/ $NEMO_ROOT/LIBS/MHC/ >> $CHEMLIBS_LOG 2>&1
+echo "### $(date) - finished rsync" >> $CHEMLIBS_LOG
+echo "Finished ChemLIBS"
 
-echo "Skipping SuperLIBS 5150"
-# echo "Starting SuperLIBS 5150"
+echo "Starting SuperLIBS 5150"
 SUPERLIBS_DIR=$DATA_ROOT/MHC.SUPERLIBS/SAMPLE\ RUNS
 SUPERLIBS_LOG=$SUPERLIBS_DIR/nightly-logs/MHC_5120-$TODAY.log
-# echo "### $(date) - starting processing" > $SUPERLIBS_LOG
-# python2.7 $SCRIPT_ROOT/process_superlibs_files.py -i $SUPERLIBS_DIR/PREPROCESSED_NO_BLR/MHC_SuperLIBS_5120 -o $SUPERLIBS_DIR/to-DEVAS/MHC_5120/prepro_no_blr -m $CHEMLIBS_DIR/COMPOSITIONS/Millennium_COMPS.xlsx >> $SUPERLIBS_LOG 2>&1
-# echo "### $(date) - finished processing, starting rsync" >> $SUPERLIBS_LOG
-# rsync -av $SUPERLIBS_DIR/to-DEVAS/MHC_5120/ $NEMO_ROOT/SuperLIBS/MHC_5120/ >> $SUPERLIBS_LOG 2>&1
-# echo "### $(date) - finished rsync" >> $SUPERLIBS_LOG
-# echo "Finished SuperLIBS 5150"
+echo "### $(date) - starting processing" > $SUPERLIBS_LOG
+python2.7 $SCRIPT_ROOT/process_superlibs_files.py -i $SUPERLIBS_DIR/PREPROCESSED_NO_BLR/MHC_SuperLIBS_5120 -o $SUPERLIBS_DIR/to-DEVAS/MHC_5120/prepro_no_blr -m $CHEMLIBS_DIR/COMPOSITIONS/Millennium_COMPS.xlsx >> $SUPERLIBS_LOG 2>&1
+echo "### $(date) - finished processing, starting rsync" >> $SUPERLIBS_LOG
+rsync -av $SUPERLIBS_DIR/to-DEVAS/MHC_5120/ $NEMO_ROOT/SuperLIBS/MHC_5120/ >> $SUPERLIBS_LOG 2>&1
+echo "### $(date) - finished rsync" >> $SUPERLIBS_LOG
+echo "Finished SuperLIBS 5150"
 
-echo "Skipping server refresh"
-# echo "Starting server refresh"
-# echo "$(wget -O- --method=POST http://nemo.p/nightly-refresh 2>&1)"
+echo "Server refresh:"
+echo "$(wget -O- --method=POST http://nemo.p/nightly-refresh 2>&1)"
 
 echo "Finished running all MHC datasets"
