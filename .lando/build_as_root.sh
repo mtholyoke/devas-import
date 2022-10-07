a12b52f4e0d1c73beb5200fc343e3230d014e5e1
Jae Proctor <jproctor@mtholyoke.edu>
2022-10-07 16:16:50 -0400

    Simplify the build for Lando

---
+++ .lando/build_as_root.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+apt update
+apt install --yes python3-h5py python3-numpy python3-openpyxl python3-yaml
--- lando/command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/bin/python3 /app/superman_server.py > /app/logs/errors.out 2>&1
